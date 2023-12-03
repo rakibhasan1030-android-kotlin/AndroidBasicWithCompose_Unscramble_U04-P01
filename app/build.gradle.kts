c4bce3b999648d64df2479ff20732de4fd7fed20
Rakib <hasan.rakib@datasoft-bd.com>
2023-12-03 16:58:44 +0600

    gradle update.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -54,11 +54,24 @@ dependencies {
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
     implementation("androidx.activity:activity-compose:1.8.1")
+
+    /*
+    Compose BOM is the recommended way to manage Compose library versions. (BOM) lets you manage all of your Compose library versions by specifying only the BOM's version.
+    Observe the following:
+    - Compose library version numbers are not specified.
+    - BOM is imported using implementation platform('androidx.compose:compose-bom:2023.06.01')
+    This is because the BOM itself has links to the latest stable versions of the different Compose libraries, in such a way that they work well together.
+    When using the BOM in your app, you don't need to add any version to the Compose library dependencies themselves.
+    When you update the BOM version, all the libraries that you're using are automatically updated to their new versions.
+    [ Note: Compose BOM is only for Compose libraries, not for other libraries such as lifecycle androidx.lifecycle library ]
+    */
     implementation(platform("androidx.compose:compose-bom:2023.03.00"))
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
+
+
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
